a45db5757133e962020cd4d8975b8d120eccba0b
Jérémy Guérin-Coquereau <jeremy@lifeaz.fr>
2025-02-19 16:40:47 +0100

    feat: react app init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FileUpload from "./FileUpload";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>CSV Uploader</h1>
+      <FileUpload />
+    </div>
+  );
+};
+
+export default App;
